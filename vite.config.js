d0ff18dee6f408fab1cfbd3fb8fe2736ddcaefca
Alexey Suprun <alexey.suprun.mail@gmail.com>
2024-06-19 11:13:53 +0200

    fix: hotfix for vite's absolute paths imports

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      components: path.resolve(__dirname, './src/components'),
+      utils: path.resolve(__dirname, './src/utils'),
+      types: path.resolve(__dirname, './src/types'),
+    },
+  },
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',
